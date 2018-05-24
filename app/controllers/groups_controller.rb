f69adb9496c11f74e18d6fe7d31e6ea131bb012a
siyuboomboom <siyu.tan.1012@hotmail.com>
2018-05-24 23:25:41 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -30,8 +30,11 @@ class GroupsController < ApplicationController
 
 	def create
 		@group = Group.new(group_params)
-		@group.save
-		redirect_to groups_path
+		if @group.save
+			redirect_to groups_path
+		else
+			render :new
+		end
 	end
 	private
 	def group_params
